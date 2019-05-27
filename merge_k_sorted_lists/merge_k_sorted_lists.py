a37bdb4cd2b6e8f81ca6e0b094a0730ac6b14437
davisr137 <davisr137@gmail.com>
2019-05-27 16:27:46 -0700

    median of k sorted lists divide and conquer solution

--- merge_k_sorted_lists/merge_k_sorted_lists.py
+++ merge_k_sorted_lists/merge_k_sorted_lists.py
@@ -1,49 +1,82 @@
 from typing import List
 
-# https://leetcode.com/problems/merge-k-sorted-lists/solution/
+# https://leetcode.com/problems/merge-k-sorted-lists/
 
 class ListNode:
     """
-    Node in a singly linked list.
+    Node in singly linked list.
     """
     def __init__(self, x):
         self.val = x
         self.next = None
 
-def arrayToLinkedList(array: List[int]) -> List[ListNode]:
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
     """
-    Make a linked list from an array.
+    Merge two sorted linked lists.
     """
-    L = len(array)
-    nodes = [ListNode(val) for val in array]
-    for i, node in enumerate(nodes):
-        if i + 1 < L:
-            node.next = nodes[i+1]
-    root = nodes[0]
+    # Initialize root to None
+    root = None
+    # Iterate while we have values in either list
+    while l1 or l2:
+        # List 1 is empty, use list 2
+        if not l1:
+            node = ListNode(l2.val)
+            l2 = l2.next
+        # List 2 is empty, use list 1
+        elif not l2:
+            node = ListNode(l1.val)
+            l1 = l1.next
+        # Add smaller value to list
+        else:
+            if l1.val <= l2.val:
+                node = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                node = ListNode(l2.val)
+                l2 = l2.next
+        # If root not made yet, make it
+        if not root:
+            root = node
+        # Update next field of previous node
+        else:
+            prev.next = node
+        # Update previous node
+        prev = node
+    # Return root of new list
     return root
 
-def arraysToLinkedLists(arrays: List[List[int]]) -> List[ListNode]:
+def passListOfLists(lists: List[ListNode]) -> List[ListNode]:
     """
-    Make several linked lists from several arrays.
+    Execute one pass through a list of sorted lists where 
+    we merge lists two at a time. 
     """
-    return [arrayToLinkedList(array) for array in arrays]
+    L = len(lists)
+    lists_merged = []
+    i = 0
+    while i < L:
+        # Only one list left; add it
+        if i == L-1:
+            lists_merged += [lists[i]]
+        # At least two lists left; merge next two and add result
+        else:
+            lists_merged += [mergeTwoLists(lists[i], lists[i+1])]
+        i += 2
+    return lists_merged
 
-def takeSecond(elem):
+def mergeKLists(lists: List[ListNode]):
     """
-    Take second element in list, array, tuple.
+    Merge K sorted lists using a divide and conquer approach.
+    At each step combine sets of two lists to halve or nearly 
+    halve the number of lists. Continue until there is only one
+    list left.
     """
-    return elem[1]
-
-def getValsFromRoots(lls: List[ListNode]):
-    """
-    Get values of nodes from roots of sorted lists.
-    """
-    vals = []
-    for i, root in enumerate(lls):
-        vals.append(tuple([i, root.val]))
-    vals = sorted(vals, key=takeSecond)
-    return vals
+    if not lists:
+        return []
+    while len(lists) > 1:
+        lists = passListOfLists(lists)
+    root = lists[0]
+    return root
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        pass        
+        return mergeKLists(lists)
