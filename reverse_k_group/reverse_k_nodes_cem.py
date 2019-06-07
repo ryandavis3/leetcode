cbf2825e49fe874817ebded72fde2567e82e88a8
davisr137 <davisr137@gmail.com>
2019-06-07 10:28:48 -0700

    reverse k nodes constant memory

--- reverse_k_group/reverse_k_nodes_cem.py
+++ reverse_k_group/reverse_k_nodes_cem.py
@@ -51,19 +51,26 @@ def reverseKGroup(head: ListNode, k: int) -> ListNode:
     """
     Given a linked list, reverse the nodes of a linked list
     k at a time and return its modified list.
+    
+    Approach uses O(k) space.
     """
     # Degenerate case - no head passed
     if not head:
         return []
+    # If k=1, return head of list as-is
     if k == 1:
         return head
     node = head
     Lprev = None
     nextnode = node
+    # Iterate while there are still nodes left in the list
     while nextnode:
+        # Make list of next k  nodes
         L = arrayOfListNodes(node, k)
+        # List for "swapped" nodes
         Lswap = []
-        # At least k more nodes left.
+        # At least k more nodes left. Make next fields point
+        # in opposite direction
         if len(L) == k:
             Li = []
             prev = L[0]
@@ -75,15 +82,18 @@ def reverseKGroup(head: ListNode, k: int) -> ListNode:
                 Li = [node] + Li
                 prev = node
             Lswap += Li
-        # Less than k nodes left; add to list
+        # Less than k nodes left; add to new linked list
+        # without swaps.
         else:
             Lswap += L
             nextnode = None
+        # "connect" previous and current swapped lists together
         if Lprev:
             tail1 = Lprev[-1]
             head2 = Lswap[0]
             tail1.next = head2
         else:
+            # Update pointer to head of list to reflect swap
             head = Lswap[0]
         Lprev = Lswap
         node = nextnode
