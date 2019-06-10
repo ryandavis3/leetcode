2d1608589fc193ca1853aa44a393ceda83f900ff
davisr137 <davisr137@gmail.com>
2019-06-09 21:18:44 -0700

    n queens

--- nqueens/nqueens.py
+++ nqueens/nqueens.py
@@ -3,7 +3,20 @@ from typing import List
 # N-Queens Problem
 # https://leetcode.com/problems/n-queens/
 
-def isSafe(board: List[List[int]], row: int, col: int) -> bool:
+def emptyBoard(N: int) -> List[List[int]]:
+    """
+    Build empty NxN board.
+    """
+    board = []
+    for _ in range(N):
+        board += [[0]*N]
+    return board
+
+def printBoard(board: List[List[int]]) -> None:
+    for line in board:
+        print(line)
+
+def isSafe(board: List[List[int]], row: int, col: int, N: int) -> bool:
     """
     Return true if queens can safely be placed on board
     at row, col. Otherwise return False.
@@ -11,7 +24,6 @@ def isSafe(board: List[List[int]], row: int, col: int) -> bool:
     This function should be called when queens have been
     placed from column 0 to col-1.
     """
-    N = len(board)
     # Queens in same row
     for j in range(col):
         if board[row][j]:
@@ -25,7 +37,7 @@ def isSafe(board: List[List[int]], row: int, col: int) -> bool:
         i += 1
         j -= 1
     # Queens on left upper diagonal
-    i = row + 1
+    i = row - 1
     j = col - 1
     while i >= 0 and j >= 0:
         if board[i][j]:
@@ -33,6 +45,23 @@ def isSafe(board: List[List[int]], row: int, col: int) -> bool:
         i -= 1
         j -= 1
     return True
+
+def solve(board: List[List[int]], col: int, N: int):
+    """
+    Recursively solve N-Queens problem by placing a 
+    queen in column col.
+    """
+    # If all queens are placed, return True
+    if col >= N:
+        return True
+    # Try to place queen in all rows one by one
+    for i in range(N):
+        if isSafe(board, i, col, N):
+            board[i][col] = 1
+            if solve(board, col+1, N):
+                return True
+            board[i][col] = 0
+    return False
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
