c29eabb2f9f8d9f5ae677edfd550df7b79caa56a
davisr137 <davisr137@gmail.com>
2019-07-30 17:59:01 -0700

    preorder inorder

---
+++ binary_tree_preorder_inorder/preorder_inorder.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    """
+    Binary tree node.
+    """
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def buildTree(preorder: List[int], inorder: List[int]) -> TreeNode:
+    """
+    Build tree from pre-order and inorder traversals.
+    """
+    pass
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        pass
