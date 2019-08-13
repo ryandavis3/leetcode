2078deac3911eabf347f8219d8847f9d49f50489
davisr137 <davisr137@gmail.com>
2019-08-13 16:17:37 -0700

    sudoku solver

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -6,6 +6,8 @@ from typing import List, Dict, Set
 # Use dict / set representation of board to allow
 # O(1) time checking of constraints.
 
+ALL_VALS = set(['1','2','3','4','5','6','7','8','9'])
+
 def representRows(board: List[List[str]]) -> Dict:
     """
     Represent rows on board using dict of sets.
@@ -18,6 +20,26 @@ def representRows(board: List[List[str]]) -> Dict:
             if val != '.':
                 R[i].add(val)
     return R
+
+def neededValues(vals: Dict) -> Dict:
+    """
+    Get needed values for rows and columns.
+    """
+    needed = {}
+    for i in range(9):
+        needed[i] = ALL_VALS - vals[i]
+    return needed
+
+def neededValuesSubBoard(vals: Dict) -> Dict:
+    """
+    Get needed values for sub-board.
+    """
+    needed = {}
+    for i in range(3):
+        needed[i] = {}
+        for j in range(3):
+            needed[i][j] = ALL_VALS - vals[i][j]
+    return needed
 
 def representCols(board: List[List[str]]) -> Dict:
     """
@@ -82,39 +104,35 @@ class Board:
     Class for Sudoku board.
     """
     def __init__(self, board: List[List[str]], rows: Dict, 
-            cols: Dict, sub_boards: Dict, empty: Set):
+            cols: Dict, sub_boards: Dict, empty: Set, needed_rows: Dict,
+            needed_cols: Dict, needed_sub: Dict):
         """
         Constructor.
         """
         self.board = board
-        # Rows
-        if not rows:
-            self.rows = representRows(board)
-        else:
-            self.rows = rows
-        # Columns
-        if not cols:
-            self.cols = representCols(board)
-        else:
-            self.cols = cols
-        # Sub-boards
-        if not sub_boards:
-            self.sub_boards = representSubBoards(board)
-        else:
-            self.sub_boards = sub_boards
-        # Empty cells
-        if not empty:
-            self.empty = getEmptyCells(board)
-        else:
-            self.empty = empty
+        self.rows = rows
+        self.cols = cols
+        self.sub_boards = sub_boards
+        self.empty = empty
+        self.empty_list = list(empty)
+        self.needed_rows = needed_rows
+        self.needed_cols = needed_cols
+        self.needed_sub = needed_sub
 
     @classmethod
     def from_board(cls, board: List[List[str]]):
+        """
+        Instantiate class from only board values.
+        """
+        # Represent rows, cols, sub-boards, empty
         rows = representRows(board)
         cols = representCols(board)
         sub_boards = representSubBoards(board)
         empty = getEmptyCells(board)
-        return cls(board, rows, cols, sub_boards, empty)
+        needed_rows = neededValues(rows)
+        needed_cols = neededValues(cols)
+        needed_sub = neededValuesSubBoard(sub_boards)
+        return cls(board, rows, cols, sub_boards, empty, needed_rows, needed_cols, needed_sub)
 
     def validMove(self, i: int, j: int, val: str) -> bool:
         """
@@ -134,6 +152,10 @@ class Board:
             return False
         # All constraints satisfied -> return True!
         return True
+
+    @property
+    def isFull(self):
+        return len(self.empty) == 0
 
     def makeMove(self, i: int, j: int, val: str):
         """
@@ -155,15 +177,61 @@ class Board:
         # Update board
         board = copy.deepcopy(self.board)
         board[i][j] = val
+        # Update needed rows
+        needed_rows = copy.deepcopy(self.needed_rows)
+        needed_rows[i].remove(val)
+        # Update needed columns
+        needed_cols = copy.deepcopy(self.needed_cols)
+        needed_cols[j].remove(val)
+        # Update sub-boards
+        needed_sub = copy.deepcopy(self.needed_sub)
+        needed_sub[sub_i][sub_j].remove(val)
         # Return new board
         kwargs = {
             'board' : board,
             'rows' : rows,
             'cols' : cols,
             'sub_boards' : sub_boards,
-            'empty' : empty
+            'empty' : empty,
+            'needed_rows' : needed_rows,
+            'needed_cols' : needed_cols,
+            'needed_sub' : needed_sub,
         }
         return Board(**kwargs)
+
+    def validNumbers(self, i: int, j: int):
+        sub_i = subBoardIndex(i)
+        sub_j = subBoardIndex(j)
+        return self.needed_rows[i] & self.needed_cols[j] & self.needed_sub[sub_i][sub_j]
+
+def findEmpty(board: Board):
+    for i in range(9):
+        for j in range(9):
+            if board.board[i][j] == '.':
+                return [i, j]
+
+def solve(board: Board):
+    """
+    Solve sudoku using backtracking.
+    """
+    # Finished sudoku board!!
+    if board.isFull:
+        return board
+    # Choose (i, j) to which to add number
+    [i, j] = findEmpty(board)
+    cand = board.validNumbers(i, j)
+    # Consider each possible number to add 
+    for val in cand:
+        # Move is valid
+        if board.validMove(i, j, str(val)):
+            # Make move and recursively solve
+            board_next = board.makeMove(i, j, str(val))
+            board_next = solve(board_next)
+            # Valid board return it!
+            if board_next is not False:
+                return board_next
+    # No valid board found - return False
+    return False
 
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
