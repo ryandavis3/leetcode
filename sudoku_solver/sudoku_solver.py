e1b707707d5b73b71e255044ecff506073151735
davisr137 <davisr137@gmail.com>
2019-08-13 14:03:49 -0700

    wip checkpoint

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,4 +1,5 @@
-from typing import List, Dict
+import copy
+from typing import List, Dict, Set
 
 # https://leetcode.com/problems/sudoku-solver/
 
@@ -60,19 +61,61 @@ def subBoardIndex(k: int) -> List:
     else:
         return 2
 
+def getEmptyCells(board: List[List[str]]) -> Set:
+    """
+    Return set of empty cells from board.
+    """
+    empty = set()
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == '.':
+                empty.add(tuple([i, j]))
+    return empty
+
+## TODO: Initialize list of empty cells. Update list when
+## number is added. 
+## TODO: Member function to make move (add number) on board.
+## TODO: Backtracking to find solution. 
+
 class Board:
     """
     Class for Sudoku board.
     """
-    def __init__(self, board: List[List[str]]):
+    def __init__(self, board: List[List[str]], rows: Dict, 
+            cols: Dict, sub_boards: Dict, empty: Set):
         """
         Constructor.
         """
         self.board = board
-        self.rows = representRows(board)
-        self.cols = representCols(board)
-        self.sub_boards = representSubBoards(board)
-    
+        # Rows
+        if not rows:
+            self.rows = representRows(board)
+        else:
+            self.rows = rows
+        # Columns
+        if not cols:
+            self.cols = representCols(board)
+        else:
+            self.cols = cols
+        # Sub-boards
+        if not sub_boards:
+            self.sub_boards = representSubBoards(board)
+        else:
+            self.sub_boards = sub_boards
+        # Empty cells
+        if not empty:
+            self.empty = getEmptyCells(board)
+        else:
+            self.empty = empty
+
+    @classmethod
+    def from_board(cls, board: List[List[str]]):
+        rows = representRows(board)
+        cols = representCols(board)
+        sub_boards = representSubBoards(board)
+        empty = getEmptyCells(board)
+        return cls(board, rows, cols, sub_boards, empty)
+
     def validMove(self, i: int, j: int, val: str) -> bool:
         """
         Return True if we can place val at position (i, j), 
@@ -92,6 +135,35 @@ class Board:
         # All constraints satisfied -> return True!
         return True
 
+    def makeMove(self, i: int, j: int, val: str):
+        """
+        Add value val to index (i, j) on board.
+        """
+        # Update row sets
+        rows = copy.deepcopy(self.rows)
+        rows[i].add(val)
+        # Update column sets
+        cols = copy.deepcopy(self.cols)
+        cols[j].add(val)
+        # Update sub-boards
+        sub_i = subBoardIndex(i)
+        sub_j = subBoardIndex(j)
+        sub_boards = copy.deepcopy(self.sub_boards)
+        sub_boards[sub_i][sub_j].add(val)
+        # Update empty cells
+        empty = self.empty - set([tuple([i, j])])
+        # Update board
+        board = copy.deepcopy(self.board)
+        board[i][j] = val
+        # Return new board
+        kwargs = {
+            'board' : board,
+            'rows' : rows,
+            'cols' : cols,
+            'sub_boards' : sub_boards,
+            'empty' : empty
+        }
+        return Board(**kwargs)
 
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
