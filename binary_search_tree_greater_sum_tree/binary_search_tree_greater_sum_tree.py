069804706677b6d2e787ab09acee89104368f10b
davisr137 <davisr137@gmail.com>
2019-09-03 12:09:48 -0700

    binary search tree to greater sum tree

--- binary_search_tree_greater_sum_tree/binary_search_tree_greater_sum_tree.py
+++ binary_search_tree_greater_sum_tree/binary_search_tree_greater_sum_tree.py
@@ -1,52 +1,37 @@
 from typing import List
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-def bstToGst(root: TreeNode, _sum: int) -> TreeNode:
+class TreeNode:
     """
-    Given the root of a binary search tree with distinct values,
-    modify it so every node has a new value equal to the sum 
-    of the values of the original tree that are greater than
-    or equal to node.val.
+    Binary tree node.
     """
-    # Search right subtree
-    if root.right:
-        right = bstToGst(root.right)    
-        root.val += root.right.val
-    
-    # Search left subtree
-    if root.left:
-        root.left.val += root.val 
-        bstToGst(root.left)
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-
-def bstToGst2(root: TreeNode, prev_sum: int) -> List:
+def bstToGst(root: TreeNode, prev_sum: int) -> int:
     """
     Given the root of a binary search tree with distinct values,
     modify it so every node has a new value equal to the sum
     of the values of the original tree that are greater than
     or equal to node.val.
-    """
-    if not root:
-        return [None, prev_sum]
 
+    Perform a reverse inorder traversal.
+    """
+    # Search right subtree
     if root.right:
-        [right_node, right_sum] = bstToGst2(root.right, prev_sum)
-        root.val += right_sum
-    
-    root.val += prev_sum
-    
-    prev_sum = root.val
-    [left_node, left_sum] = bstToGst2(root.left, prev_sum)
-    
-    return [root, left_sum]
+        root.val += bstToGst(root.right, prev_sum)
+    else:
+        root.val += prev_sum
+    # Search left subtree
+    if root.left:
+        left_sum = bstToGst(root.left, root.val)
+    else:
+        left_sum = root.val
+    # Return sum of entire subtree to parent
+    return left_sum
 
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        [root, v] = bstToGst2(root, 0)
+        val = bstToGst(root, 0)
         return root
