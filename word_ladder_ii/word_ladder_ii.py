e6e21e080e75477b79d329484ff86219824f9970
davisr137 <davisr137@gmail.com>
2019-09-30 15:30:07 -0700

    word ladder Dijkstra solution

--- word_ladder_ii/word_ladder_ii.py
+++ word_ladder_ii/word_ladder_ii.py
@@ -3,29 +3,9 @@ from typing import List, Set, Dict
 
 # https://leetcode.com/problems/word-ladder-ii/
 
-def findLadders(beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-    """
-    Given two words and a dictionary's word list, find all transformation
-    sequence(s) such that only one letter can be changed at a time
-    and each transformed word must exist in the word list.
-    """
-    # Return empty list if beginning / end words not in word list
-    wordList = set(wordList)
-    wordList.add(beginWord)
-    adj = buildAdjList(wordList)
-    words = search(beginWord, endWord, wordList, adj)
-    if not words: 
-        return []
-    words.sort(key=len)
-    L = len(words[0])
-    return [w for w in words if len(w) == L]
-
-# Use a radix tree to solve??
-# Try using BFS first?? 
-
 def oneLetterDiff(a: str, b: str) -> bool:
     """
-    Return True if strings a and b have exactly one 
+    Return True if strings a and b have exactly one
     letter different.
     """
     diff = 0
@@ -40,94 +20,111 @@ def oneLetterDiff(a: str, b: str) -> bool:
 
 def buildAdjList(wordList: Set):
     """
-    Build adjacency list for words.
+    Build adjacency list for words. Two words are adjacent if
+    they differ by exactly one character.
     """
     adj = {}
     for w1 in wordList:
-        adj[w1] = []
+        adj[w1] = set()
         for w2 in wordList:
             if oneLetterDiff(w1, w2):
-                adj[w1] += [w2]
+                adj[w1].add(w2)
     return adj
 
+# Use two BFS traversals, one from begin and one from end??
 
-def search(beginWord: str, endWord: str, wordList: Set, adj: Set) -> List[List[str]]:
-    
-    # Word not available - return empty list
-    if beginWord not in wordList:
-        return []
-    # Finished!
-    if beginWord == endWord:
-        return [[beginWord]]
-    words = []
-    words_adj = adj[beginWord]
-    wordList.remove(beginWord)
-    for word in words_adj:
-        nextWords = search(word, endWord, wordList, adj)
-        for n in nextWords:
-            words += [[beginWord] + n]
-    return words
+# BFS one step at a time clearly defined??
 
-def step(state: Dict, adj: Dict):
+def search(begin: str, end: str, words: Set):
+    """
+    Use Dijkstra's algorithm to find all paths from beginning
+    word to end word.
+    """
+    # Dictionaries for distance from beginning word and 
+    # previous word in path.
+    dist = {}
+    prev = {}
+    for w in words:
+        dist[w] = 10**10
+        prev[w] = {}
+    dist[begin] = 0
+    # Word adjacency list 
+    adj = buildAdjList(words)
+    # Traverse graph
+    Q = [begin]
+    while Q:
+        word = Q.pop(0)
+        for aw in adj[word]:
+            alt = dist[word] + 1
+            if alt < dist[aw]:
+                dist[aw] = alt
+                prev[aw] = {word}
+                Q += [aw]
+            elif alt == dist[aw]:
+                prev[aw].add(word)
+                Q += [aw]
+        removeWordAdj(adj, word)
+    # Return completed distance and previous word dicts
+    return [dist, prev]
 
-    if state['begin'] not in state['words']:
-        return None
-    
-    if state['begin'] == state['end']:
-        state['sequence'] += [state['begin']]
-        state['complete'] = True
-        return [state]
-    
-    # Adjacent words
-    words_adj = adj[state['begin']]
-
-    states_next = []
-    for word in words_adj:
-        if word in state['words']:
-            state_next = copy.deepcopy(state)  
-            state_next['words'] = state_next['words'] - {state['begin']}
-            state_next['sequence'] += [state['begin']]
-            state_next['begin'] = word
-            states_next += [state_next]
-
-    return states_next
-
-# Memoize!
-
-def steps(states: List[Dict], adj: Dict):
-
-    print(states)
-    states_next = []
+def recoverLadders(begin: str, end: str, prev: Dict):
+    """
+    Recover paths from beginning word to end word using 
+    predecessor words.
+    """
+    # Start with end word
+    ladders = [[end]]
+    words = [end]
+    # Fill out ladders until we reach beginning word
     complete = False
-    for state in states:
-        state_next = step(state, adj)
-        if state_next is not None:
-            states_next += state_next
-            for sn in state_next:
-                if sn['complete']:
+    while not complete:
+        ladders_next = []
+        words_next = []
+        for i, ladder in enumerate(ladders):
+            pws = prev[words[i]]
+            for pw in pws:
+                ladders_next += [ladder + [pw]]
+                words_next += [pw]
+                if pw == begin:
                     complete = True
-    
-    if complete:
-        states_next = [sn for sn in states_next if sn['complete']]
+        ladders = ladders_next
+        words = words_next
+    # Get unique paths, reverse order
+    ladders = list(set([tuple(l) for l in ladders]))
+    ladders = [list(l) for l in ladders]
+    ladders = [l[::-1] for l in ladders]
+    return ladders
 
-    return states_next
+def removeWordAdj(adj: Dict, word):
+    """
+    Remove word from adjacency lists.
+    """
+    for w in adj:
+        if word in adj[w]:
+            adj[w].remove(word)
 
-def findLadders2(beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-
-    wordList = set(wordList)
-    wordList.add(beginWord)
-    state = {'begin': beginWord, 'end': endWord, 'words': wordList, 'complete': False, 'sequence': []}
-    states = [state]
-    adj = buildAdjList(wordList)
-    while True:
-        states = steps(states, adj)
-        if not states:
-            return []
-        if states[0]['complete']:
-            states = [s['sequence'] for s in states]
-            return states
-
+def findLadders(self, begin: str, end: str, words: List[str]) -> List[List[str]]:
+    """
+    Given two words (begin and end) and a dictionary's word list, find
+    all shortest transformation sequence(s) such that only one
+    letter can be changed at a time and each transformed word must 
+    exist in the word list.
+    """
+    # Represent word list as set rather than list
+    words = set(words)
+    words.add(begin)
+    # End word not in list! No possible paths!
+    if end not in words:
+        return []
+    # Find all paths in word "graph"
+    [dist, prev] = search(begin, end, words)
+    # Did not reach end -> return empty list
+    if not prev[end]:
+        return []
+    # Recover ladders from predecessors
+    ladders = recoverLadders(begin, end, prev)
+    return ladders
 
 class Solution:
-    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        return findLadders2(beginWord, endWord, wordList)
+    def findLadders(self, begin: str, end: str, words: List[str]) -> List[List[str]]:
+        return findLadders(begin, end, words)
