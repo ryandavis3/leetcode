a72c0bc02d288591c70f61d1d7cefa95f9193433
Ryan Davis <rdavis@flexport.com>
2021-04-05 13:56:40 -0400

    longest happy string

--- longest_diverse_string/longest_diverse_string.py
+++ longest_diverse_string/longest_diverse_string.py
@@ -1,25 +1,68 @@
 class PriorityQueue:
-    def __init__(self, a: int, b: int, c: int):
-        # Sort values
-        self.L = [("a", a), ("b", b), ("c", c)]
-        self.L = sorted(L, key = lambda x: x[1], reverse=True)
-    def pop() -> str:
-        s = self.L[0]
-        self.L[0] -= 1
-    def continue(self) -> bool:
-        for i in range(3):
-            if self.L[i][1] == 0:
-                return False
-        return True
 
-def longestDiverseString(self, a: int, b: int, c: int) -> str:
-    pq = PriorityQueue(a, b, c)
-    s = ""
+    def __init__(self, a: int, b: int, c: int):
+        """
+        Constructor.
+        """
+        # Sort values
+        self.L = [["a", a], ["b", b], ["c", c]]
+        self.L = sorted(self.L, key = lambda x: x[1], reverse=True)
+        self.consecutive = 0
+        self.string = ""
+
+    def pop(self, index: int=0) -> str:
+        """
+        Pop 'top' element from the queue.
+        """
+        s = self.L[index][0]
+        self.L[index][1] -= 1
+        self.L = sorted(self.L, key = lambda x: x[1], reverse=True)
+        return s
     
+    def go(self) -> bool:
+        """
+        Return True if we can continue, else return False.
+        """
+        skip = None
+        if self.consecutive == 2:
+            skip = self.string[-1]
+        for i in range(3):
+            if self.L[i][0] == skip:
+                continue
+            if self.L[i][1] > 0:
+                return True
+        return False
+
+    def add(self):
+        skip = None
+        if self.consecutive == 2:
+            skip = self.string[-1]
+            self.consecutive = 0
+        else:
+            self.consecutive += 1
+        if self.L[0][0] == skip:    
+            self.string += self.pop(index=1)
+        else:
+            self.string += self.pop(index=0)
+
+def longestDiverseString(a: int, b: int, c: int) -> str:
+    """
+    Get longest 'diverse' string satisfying requirements.
+    """
+    pq = PriorityQueue(a, b, c)
+    while pq.go():
+        pq.add()
+    return pq.string
+
 
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        pass
+        return longestDiverseString(a, b, c)
+
 
 if __name__ == '__main__':
-
+    a = 1 
+    b = 3
+    c = 5
+    s = longestDiverseString(a, b, c)
+    print(s)
