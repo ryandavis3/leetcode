9b630cafd05a2ee0b4f659ee1d64ca311cfb29ac
Ryan Davis <rdavis@flexport.com>
2021-04-06 20:49:18 -0400

    min cost tickets

--- min_cost_tickets/min_cost_tickets.py
+++ min_cost_tickets/min_cost_tickets.py
@@ -1,7 +1,13 @@
 from typing import List
 
 def subtractDays(days: List[int], start: int, diff: int) -> int:
-    pass
+    day = start - diff
+    days_set = set(days)
+    while day not in days_set:
+        day -= 1
+        if day < 1:
+            return -1
+    return days.index(day)
 
 def mincostTickets(days: List[int], costs: List[int]) -> int:
     L = len(days)
@@ -15,20 +21,28 @@ def mincostTickets(days: List[int], costs: List[int]) -> int:
         for jj in range(len(days)):
             options = []
             options += [table[i-1][jj]]
-            if jj + 1 < lengths[i]:
+            if days[jj] <= lengths[i]:
                 options += [costs[i]]
             else:
-                breakpoint()
-                options += [table[i][jj - lengths[i]] + costs[i]]
+                for ii in range(i):
+                    if i == 1 and jj == 4:
+                        pass
+                    col = subtractDays(days, days[jj], lengths[ii])
+                    if col != -1:
+                        options += [table[i][col] + costs[ii]]
             table[i][jj] = min(options)
-
+    breakpoint()
+    return table[-1][-1]
 
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        pass
+        return mincostTickets(days, costs)
 
 
 if __name__ == "__main__":
     days = [1,4,6,7,8,20]
     costs = [2,7,15]
+    #result = mincostTickets(days, costs)
+    days = [1,4,6,9,10,11,12,13,14,15,16,17,18,20,21,22,23,27,28]
+    costs = [3,13,45]
     result = mincostTickets(days, costs)
