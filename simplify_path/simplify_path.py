0ac4067883fd7b7d3e43aadffba597298444b60e
davisr137 <davisr137@gmail.com>
2023-10-20 13:46:09 -0700

    simplify

--- simplify_path/simplify_path.py
+++ simplify_path/simplify_path.py
@@ -6,15 +6,19 @@ DOUBLE_DOT = '..'
 
 
 def _resolve_double_dot(strings: List[str]) -> List[str]: 
+    # resolve "internal" double dots
     i = 0
     while i < len(strings):
         if i + 1 < len(strings) and strings[i + 1] == DOUBLE_DOT:
             strings = strings[:i] + strings[i+2:]
-            i -= 2
+            i = max(0, i - 2)
         else:
             i += 1
+    # remove leading double dot
+    if len(strings) > 0:
+        while strings[0] == DOUBLE_DOT:
+            strings = strings[1:]
     return strings
-
 
 
 def simplify_path(path: str) -> str:
@@ -59,4 +63,8 @@ class TestSimplify(unittest.TestCase):
         output = simplify_path(path=path)
         self.assertEqual(output, expected_output)
 
-
+    def test_5(self) -> None:
+        path = "/a/../../b/../c//.//"
+        expected_output = "/c"
+        output = simplify_path(path=path)
+        self.assertEqual(output, expected_output)
