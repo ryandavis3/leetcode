744fe36262e4bc1cbe87f1cbd348a0dbcf08a45d
davisr137 <davisr137@gmail.com>
2023-10-22 15:03:08 -0700

    word

--- word_filter/word_filter.py
+++ word_filter/word_filter.py
@@ -1,14 +1,29 @@
 from typing import Dict, List
 
 
+def add_word_to_dict(_dict: Dict, word: str, index: int):
+    # Word has only one or zero letters
+    if len(word) <= 1:
+        _dict[word] = index
+    # First letter
+    letter = word[0]
+    # Remaining word
+    remaining = word[1:]
+    # Letter not in dictionary -> add it
+    if letter not in _dict:
+        _dict[letter]: Dict = {}
+    # Recursively move to the next 
+    add_word_to_dict(_dict=_dict[letter], word=remaining)
+
+
 class WordTree:
 
     def __init__(self):
         self.word_tree: Dict = {}
 
-    def add_word(self, word: str) -> None:
-        letter = word[0]
-        remaining = word[1:]
+    def add_word(self, word: str, index: int) -> None:
+        add_word_to_dict(_dict=self.word_tree, word=word, index=index)
+
 
 
 class WordFilter:
