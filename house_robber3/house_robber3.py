11c9f81a06278722c0bdeae5fc8a35b2d0365a6f
davisr137 <davisr137@gmail.com>
2023-10-23 12:54:27 -0700

    house robber

--- house_robber3/house_robber3.py
+++ house_robber3/house_robber3.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import unittest
+from typing import List, Optional
 
 
 class TreeNode:
@@ -8,6 +9,25 @@ class TreeNode:
         self.right = right
 
 
+def get_max_money(node: TreeNode, include_head: bool) -> int:
+    node_value = node.val if include_head else 0
+    if node.left is None and node.right is None:
+        return node_value
+    if node.left is None and node.right is not None:
+        value = node_value + get_max_money(node=node.right, include_head=False)
+        return value
+    if node.left is not None and node.right is None:
+        value = node_value + get_max_money(node=node.left, include_head=False)
+        return value
+
+
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        pass
+        return get_max_money(node=root, include_head=True)
+
+
+#def build_tree_from_root(root: List[int]) -> TreeNode:
+
+
+class TestRob(unittest.TestCase):
+    pass
