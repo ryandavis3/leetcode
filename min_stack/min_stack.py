bbb4259f3c504402ae3c809871c23335d9cf74fb
Ryan Davis <rdavis@flexport.com>
2023-10-25 11:19:12 -0700

    min stack

--- min_stack/min_stack.py
+++ min_stack/min_stack.py
@@ -4,20 +4,26 @@ from typing import Optional
 
 
 class Node:
-    def __init__(self, value: int, prev):
+    def __init__(self, value: int, prev, node_id: str):
         self.value = value
         self.prev = prev
-        self.uuid = str(uuid.uuid4())
+        self.node_id = node_id
 
 
 class MinStack:
 
     def __init__(self):
         self._head = None
+        self._min = None
 
     def push(self, val: int) -> None:
-        node = Node(value=val, prev=self._head)
+        node_id = str(uuid.uuid4())
+        node = Node(value=val, prev=self._head, node_id=node_id)
         self._head = node
+        if self._min is None:
+            self._min = Node(value=val, prev=None, node_id=node_id)
+        elif val < self._min.value:
+            self._min = Node(value=val, prev=self._min, node_id=node_id)
 
     def pop(self) -> None:
         if self._head is None:
