fe2a516f095dd58b1583259588ea75ff93660292
Ryan Davis <rdavis@flexport.com>
2023-10-25 11:00:09 -0700

    min stack

--- min_stack/min_stack.py
+++ min_stack/min_stack.py
@@ -1,12 +1,32 @@
+import unittest
+from typing import Optional
+
+
+class Node:
+    def __init__(self, value: int, prev):
+        self.value = value
+        self.prev = prev
+
+
 class MinStack:
 
     def __init__(self):
+        self._head = None
 
     def push(self, val: int) -> None:
+        node = Node(value=val, prev=self._head)
+        self._head = node
 
     def pop(self) -> None:
+        if self._head is None:
+            raise ValueError('No head available!')
+        self._head = self._head.prev
 
     def top(self) -> int:
+        if self._head is None:
+            raise ValueError('No head available!')
+        top_value = self._head.value
+        return top_value
 
     def getMin(self) -> int:
         # Get the minimum element in the stack
@@ -18,4 +38,16 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+class TestMinStack(unittest.TestCase):
+
+    def test_top(self) -> None:
+        stack = MinStack()
+        stack.push(val=1)
+        stack.push(val=2)
+        top = stack.top()
+        self.assertEqual(top, 2)
+        stack.pop()
+        top = stack.top()
+        self.assertEqual(top, 1)
 
