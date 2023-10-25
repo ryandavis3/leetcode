f314e7e570d5b1534cb037c04b61f565759f4cb0
Ryan Davis <rdavis@flexport.com>
2023-10-25 10:47:49 -0700

    min stack

---
+++ min_stack/min_stack.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+
+    def push(self, val: int) -> None:
+
+    def pop(self) -> None:
+
+    def top(self) -> int:
+
+    def getMin(self) -> int:
+        # Get the minimum element in the stack
+        pass
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
