e691864c7d196348f81f4800cdc5f39021911a53
Ryan Davis <rdavis@flexport.com>
2023-10-29 20:57:33 -0700

    max

--- max_value_bounded/max_value_bounded.py
+++ max_value_bounded/max_value_bounded.py
@@ -1,11 +1,13 @@
 from math import ceil
 
+
 def max_sum_with_value(n: int, index: int, value: int) -> int:
     n_left = index
     left_sum = n_left * (value - 1 + value - n_left) / 2
     n_right = n - index - 1
     right_sum = n_right * (value - 1 + value - n_right) / 2
     return value + left_sum + right_sum
+
 
 class Solution:
     def maxValue(self, n: int, index: int, maxSum: int) -> int:
