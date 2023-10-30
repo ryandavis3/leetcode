f39b433a418999f14da024d098d15e18aa64c7d8
Ryan Davis <rdavis@flexport.com>
2023-10-29 18:34:13 -0700

    max

--- predict_winner/predict_winner.py
+++ predict_winner/predict_winner.py
@@ -1,4 +1,20 @@
-from typing import List
+from typing import Dict, List, Tuple
+
+
+class MemoizedMaxPoints:
+    def __init__(self):
+        self.max_points: Dict = {}
+
+    def get(self, nums_sum: List[int], turn: bool) -> Optional[int]:
+        key = (tuple(nums_sum), turn)
+        if key in self.max_points:
+            return self.max_points[key]
+        return None
+
+    def put(self, nums_sum: List[int], turn: bool, points: int) -> None:
+        key = (tuple(nums_sum), turn)
+        self.max_points[key] = points
+
 
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
