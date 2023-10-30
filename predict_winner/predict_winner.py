6a8fb1b1e70bdd0c7e111724edbd7e6f52250f97
Ryan Davis <rdavis@flexport.com>
2023-10-29 19:21:11 -0700

    predict winner

--- predict_winner/predict_winner.py
+++ predict_winner/predict_winner.py
@@ -26,26 +26,58 @@ class GameState:
     player_two_points: int
 
 
-def predict_winner(nums: List[int], turn: bool) -> int:
-    print(nums)
-    if len(nums) == 1:
-        value = nums[0]
-        if turn:
-            return value
-        return -value
-    if len(nums) == 2:
-        difference = abs(nums[0] - nums[1])
-        if turn:
-            return difference
-        return -difference
-    first_value = nums[0]
-    last_value = nums[-1]
-    if turn:
-        choose_first_result = first_value + predict_winner(nums=nums[1:], turn=False)
-        choose_last_result = last_value + predict_winner(nums=nums[:-1], turn=False)
+def predict_winner(game_state: GameState) -> int:
+    next_turn = not game_state.turn
+    if len(game_state.nums) == 1:
+        if game_state.turn:
+            player_one_points = game_state.player_one_points + game_state.nums[0]
+            player_two_points = game_state.player_two_points
+        else:
+            player_one_points = game_state.player_one_points
+            player_two_points = game_state.player_two_points + game_state.nums[0]
+        return player_one_points - player_two_points
+    if len(game_state.nums) == 2:
+        max_points = max(game_state.nums)
+        min_points = min(game_state.nums)
+        if game_state.turn:
+            player_one_points = game_state.player_one_points + max_points
+            player_two_points = game_state.player_two_points + min_points
+        else:
+            player_one_points = game_state.player_one_points + min_points
+            player_two_points = game_state.player_two_points + max_points
+        return player_one_points - player_two_points
+    first_value = game_state.nums[0]
+    last_value = game_state.nums[-1]
+    if game_state.turn:
+        next_game_state_first = GameState(
+            nums=game_state.nums[1:],
+            turn=next_turn,
+            player_one_points=game_state.player_one_points + first_value,
+            player_two_points=game_state.player_two_points,
+        )
+        choose_first_result = predict_winner(game_state=next_game_state_first)
+        next_game_state_last = GameState(
+            nums=game_state.nums[:-1],
+            turn=next_turn,
+            player_one_points=game_state.player_one_points + last_value,
+            player_two_points=game_state.player_two_points,
+        )
+        choose_last_result = predict_winner(game_state=next_game_state_last)
     else:
-        choose_first_result = -first_value + predict_winner(nums=nums[1:], turn=True)
-        choose_last_result = -last_value + predict_winner(nums=nums[:-1], turn=True)
+        next_game_state_first = GameState(
+            nums=game_state.nums[1:],
+            turn=next_turn,
+            player_one_points=game_state.player_one_points,
+            player_two_points=game_state.player_two_points + first_value,
+        )
+        choose_first_result = predict_winner(game_state=next_game_state_first)
+        next_game_state_last = GameState(
+            nums=game_state.nums[:-1],
+            turn=next_turn,
+            player_one_points=game_state.player_one_points,
+            player_two_points=game_state.player_two_points + last_value,
+        )
+        choose_last_result = predict_winner(game_state=next_game_state_last)
     return max(choose_first_result, choose_last_result)
 
 
@@ -57,25 +89,57 @@ class Solution:
 
 class TestPredictWinner(unittest.TestCase):
     def test1(self) -> None:
-        nums = [5, 2]
-        nums1 = predict_winner(nums=nums, turn=True)
+        game_state = GameState(
+            nums=[5, 2],
+            turn=True,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums1 = predict_winner(game_state=game_state)
         self.assertEqual(nums1, 3)
-        nums2 = predict_winner(nums=nums, turn=False)
+        game_state = GameState(
+            nums=[5, 2],
+            turn=False,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums2 = predict_winner(game_state=game_state)
         self.assertEqual(nums2, -3)
 
     def test2(self) -> None:
-        nums = [4]
-        nums1 = predict_winner(nums=nums, turn=True)
+        game_state = GameState(
+            nums=[4],
+            turn=True,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums1 = predict_winner(game_state=game_state)
         self.assertEqual(nums1, 4)
-        nums2 = predict_winner(nums=nums, turn=False)
+        game_state = GameState(
+            nums=[4],
+            turn=False,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums2 = predict_winner(game_state=game_state)
         self.assertEqual(nums2, -4)
 
     def test3(self) -> None:
-        nums = [1, 5, 2]
-        nums1 = predict_winner(nums=nums, turn=True)
+        game_state = GameState(
+            nums=[1, 5, 2],
+            turn=True,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums1 = predict_winner(game_state=game_state)
         self.assertEqual(nums1, -2)
 
     def test4(self) -> None:
-        nums = [1, 5, 233, 7]
-        nums2 = predict_winner(nums=nums, turn=True)
-        self.assertEqual(nums2, -2)
+        game_state = GameState(
+            nums=[1, 5, 233, 7],
+            turn=True,
+            player_one_points=0,
+            player_two_points=0,
+        )
+        nums2 = predict_winner(game_state=game_state)
+        self.assertEqual(nums2, 222)
