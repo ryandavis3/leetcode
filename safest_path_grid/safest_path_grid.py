f2a9ac3810c8baf0f6d1626b4ddd8ae733d668e6
Ryan Davis <rdavis@flexport.com>
2023-10-31 11:33:32 -0700

    safe path

---
+++ safest_path_grid/safest_path_grid.py
@@ -0,0 +1,56 @@
+import unittest
+from typing import List, Tuple
+
+
+class Grid:
+
+    def __init__(self, grid: List[List[int]]):
+        self.grid = grid
+        self.thief_coordinates: List[Tuple] = []
+        rows = len(grid)
+        columns = len(grid[0])
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j] == 1:
+                    self.thief_coordinates += [(i, j)]
+
+    def get_minimum_manhattan_distance_to_thief(self, i: int, j: int) -> int:
+        min_distance = 10 ** 10
+        for coordinates in self.thief_coordinates:
+            a, b = coordinates
+            distance = abs(a-i) + abs(b-j)
+            if distance < min_distance:
+                min_distance = distance
+        return min_distance
+
+
+
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        pass
+
+
+class TestGrid(unittest.TestCase):
+    def test_init(self) -> None:
+        grid = [[0, 0, 1], [0, 0, 0], [0, 0, 0]]
+        grid_class = Grid(grid=grid)
+
+    def test_get_minimum_manhattan_distance_to_thief(self) -> None:
+        grid = [[0, 0, 1], [0, 0, 0], [0, 0, 0]]
+        grid_class = Grid(grid=grid)
+        test_cases = {
+            (0, 0): 2,
+            (1, 0): 3,
+            (2, 0): 4,
+            (0, 1): 1,
+        }
+        for coordinates, min_distance_expected in test_cases.items():
+            i, j = coordinates
+            min_distance = grid_class.get_minimum_manhattan_distance_to_thief(i=i, j=j)
+            self.assertEqual(min_distance, min_distance_expected)
+
+
+class TestSafestPath(unittest.TestCase):
+
+    def test2(self) -> None:
+        grid = [[0, 0, 1], [0, 0, 0], [0, 0, 0]]
