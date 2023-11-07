47fa0d85752bffc41e3ebee0ac80f412b2f0e94d
Ryan Davis <rdavis@flexport.com>
2023-11-07 11:33:57 -0800

    test

--- eliminate_max_monsters.py
+++ eliminate_max_monsters.py
@@ -30,7 +30,7 @@ def step_minute_forward(monster: Monster) -> Monster:
 def eliminate_max(monsters: List[Monster]) -> int:
     if not monsters:
         return 0
-    if monsters[0].distance == 0:
+    if monsters[0].distance <= 0:
         return 0
     next_monsters = [step_minute_forward(monster=monster) for monster in monsters[1:]]
     return 1 + eliminate_max(monsters=next_monsters)
@@ -42,10 +42,23 @@ class Solution:
 
 
 class TestEliminateMax(TestCase):
-    def test_eliminate_max(self) -> None:
+    def test_eliminate_max1(self) -> None:
         distances = [1, 3, 4]
         speeds = [1, 1, 1]
         monsters = get_monsters_from_distance_speed(distances=distances, speeds=speeds)
-        print(monsters)
         max_monsters = eliminate_max(monsters=monsters)
         self.assertEqual(max_monsters, 3)
+
+    def test_eliminate_max2(self) -> None:
+        distances = [1, 1, 2, 3]
+        speeds = [1, 1, 1, 1]
+        monsters = get_monsters_from_distance_speed(distances=distances, speeds=speeds)
+        max_monsters = eliminate_max(monsters=monsters)
+        self.assertEqual(max_monsters, 1)
+
+    def test_eliminate_max3(self) -> None:
+        distances = [3, 2, 4]
+        speeds = [5, 3, 2]
+        monsters = get_monsters_from_distance_speed(distances=distances, speeds=speeds)
+        max_monsters = eliminate_max(monsters=monsters)
+        self.assertEqual(max_monsters, 1)
