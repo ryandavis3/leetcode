0736d769037da5305c7c39c38c016208bc17fc86
Ryan Davis <rdavis@flexport.com>
2023-11-07 11:25:55 -0800

    elim

--- eliminate_max_monsters.py
+++ eliminate_max_monsters.py
@@ -1,3 +1,4 @@
+from unittest import TestCase
 from dataclasses import dataclass
 from typing import List
 
@@ -8,6 +9,16 @@ class Monster:
     speed: int
     time_to_city: int
 
+    @classmethod
+    def from_distance_speed(cls, distance: int, speed: int):
+        time_to_city = (distance + 0.0) / speed
+        return cls(distance=distance, speed=speed, time_to_city=time_to_city)
+
+
+def get_monsters_from_distance_speed(distances: List[int], speeds: List[int]) -> List[Monster]:
+    monsters = [Monster.from_distance_speed(distance=distances[i], speed=speeds[i]) for i in range(len(distances))]
+    return monsters
+
 
 def step_minute_forward(monster: Monster) -> Monster:
     distance = self.distance - speed
@@ -15,10 +26,18 @@ def step_minute_forward(monster: Monster) -> Monster:
     return Monster(distance=distance, speed=monster.speed, time_to_city=time_to_city)
 
 
-def eliminate_max(dist: List[int], speed: List[int]) -> int:
-    pass
+def eliminate_max(monsters: List[Monster]) -> int:
+    for monster in monsters:
+        if monster.distance == 0:
+            return 0
+    next_monsters = [step_minute_forward(monster=monster) for monster in monsters[1:]]
+    return 1 + eliminate_max(monsters=next_monsters)
 
 
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
         pass
+
+
+class TestEliminateMax(TestCase):
+    def test_eliminate_max(self) -> None:
