05e0c2924d40aabdd34b763ed3c2d005b8835f9e
Ryan Davis <rdavis@flexport.com>
2023-11-07 14:18:29 -0800

    rectangles

--- largest_rectangle_histogram.py
+++ largest_rectangle_histogram.py
@@ -12,16 +12,21 @@ class Rectangles:
 
     def process_bar(self, bar: int):
         height_width_new: Dict[int, int] = {}
+        height_width_new[bar] = 1
+        if bar > self.max_area:
+            self.max_area = bar
         for height, width in self._height_width.items():
             if bar >= height:
                 height_width_new[height] = width + 1
                 area = height * (width + 1)
                 if area > self.max_area:
                     self.max_area = area
-        if bar not in height_width_new:
-            height_width_new[bar] = 1
-            if bar > self.max_area:
-                self.max_area = bar
+            else:
+                if width + 1 > height_width_new[bar]:
+                    height_width_new[bar] = width + 1
+                    area = bar * (width + 1)
+                    if area > self.max_area:
+                        self.max_area = area
         return Rectangles(height_width=height_width_new, max_area=self.max_area)
 
 
@@ -51,3 +56,20 @@ class TestRectangles(TestCase):
         heights = [2, 1, 5, 6, 2, 3]
         largest_area = get_largest_rectangle_area(heights=heights)
         self.assertEqual(largest_area, 10)
+
+    def test3(self) -> None:
+        rectangles = Rectangles()
+        heights = [2, 1, 2]
+        for bar in heights:
+            rectangles = rectangles.process_bar(bar=bar)
+        self.assertEqual(rectangles._height_width, {2: 1, 1: 3})
+        self.assertEqual(rectangles.max_area, 3)
+
+    def test4(self) -> None:
+        rectangles = Rectangles()
+        heights = list(range(1, 10))
+        for bar in heights:
+            rectangles = rectangles.process_bar(bar=bar)
+            print(rectangles._height_width)
+        assert False
+
