35153097d2c03f1dbb1cc0f5450e640336a73161
Ryan Davis <rdavis@flexport.com>
2023-11-08 09:40:25 -0800

    max score subarray

---
+++ max_score_subarray.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from typing import List
+
+
+def get_score_update(min_element: int, i: int, j: int, max_score: int) -> int:
+    score = min_element * (i - j + 1)
+    if score > max_score:
+        max_score = score
+    return max_score
+
+
+def get_max_score(nums: List[int], k: int) -> int:
+    i = 0
+    j = k
+    L = len(nums)
+    min_element = min(nums[i:j+1])
+    max_score = min_element * (i - j + 1)
+    while i <= k and nums[i] == min_element:
+        i += 1
+        min_element = min(nums[i:j+1])
+        max_score = get_score_update(min_element=min_element, i=i, j=j, max_score=max_score)
+        while k <= j < L and nums[j] != min_element:
+            j += 1
+            max_score = get_score_update(min_element=min_element, i=i, j=j, max_score=max_score)
+        if j < L:
+            min_element = nums[j]
+    return max_score
+
+
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        pass
+
+
+class TestMaxScore(TestCase):
+    def test1(self) -> None:
+        max_score = get_max_score(nums=[1, 4, 3, 7, 4, 5], k=3)
+        self.assertEqual(max_score, 15)
+
+    def test2(self) -> None:
+        max_score = get_max_score(nums=[5, 5, 4, 5, 4, 1, 1, 1], k=0)
+        self.assertEqual(max_score, 15)
