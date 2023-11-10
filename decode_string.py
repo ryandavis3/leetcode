8448c2ef7893ff48d47430eb24aba8495d70b28a
Ryan Davis <rdavis@flexport.com>
2023-11-10 14:57:55 -0800

    decoded

--- decode_string.py
+++ decode_string.py
@@ -1,9 +1,40 @@
 from unittest import TestCase
 
 
+OPEN = '['
+CLOSED = ']'
+
+
 def decode_string(s: str) -> str:
+    print(s)
+    L = len(s)
+    # String is all alphabetic characters -> return it
     if s.isalpha():
         return s
+    # Check for leading digits -> if present, use as multiplier
+    if s[0].isdigit():
+        i = 1
+        while s[i].isdigit():
+            i += 1
+        multiplier = int(s[0:i])
+    else:
+        i = 0
+        multiplier = 1
+    # Get open and closed brackets
+    if s[i] == OPEN:
+        j = i + 1
+        while s[j] != CLOSED:
+            j += 1
+        decoded_in_brackets = decode_string(s=s[i+1:j])
+    # Construct decoded substring
+    decoded_substring = multiplier * decoded_in_brackets
+    # Continue processing remaining string
+    if j < L - 2:
+        decoded_remainder = decode_string(s=s[j+1:])
+    else:
+        decoded_remainder = ''
+    return decoded_substring + decoded_remainder
+
 
 
 class Solution:
@@ -28,4 +59,16 @@ class TestDecodeString(TestCase):
         s = 'abc'
         out = decode_string(s=s)
         expected = 'abc'
+        self.assertEqual(out, expected)
+
+    def test5(self) -> None:
+        s = '5[a]'
+        out = decode_string(s=s)
+        expected = 'aaaaa'
+        self.assertEqual(out, expected)
+
+    def test6(self) -> None:
+        s = '2[a]3[b]4[c]'
+        out = decode_string(s=s)
+        expected = 'aabbbcccc'
         self.assertEqual(out, expected)
