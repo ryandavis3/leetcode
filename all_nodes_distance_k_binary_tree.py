4a2af4dc330e16df278047abf720a5682112600e
Ryan Davis <rdavis@flexport.com>
2023-11-13 14:03:02 -0800

    test

--- all_nodes_distance_k_binary_tree.py
+++ all_nodes_distance_k_binary_tree.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 from unittest import TestCase
 
 
@@ -8,7 +9,10 @@ class TreeNode:
         self.right = None
 
 
-def get_distance_from_root(root: TreeNode, target: TreeNode)
+def get_path_to_target(root: TreeNode, target: TreeNode) -> List[int]:
+    if root.val == target.val:
+        return [root.val]
+
 
 
 class Solution:
@@ -17,3 +21,19 @@ class Solution:
 
 
 class TestDistanceK(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.root = TreeNode(x=1)
+        cls.node2 = TreeNode(x=2)
+        cls.node3 = TreeNode(x=3)
+        cls.node4 = TreeNode(x=4)
+        cls.node5 = TreeNode(x=5)
+        cls.root.left = cls.node2
+        cls.root.right = cls.node3
+        cls.node2.left = cls.node4
+        cls.node2.right = cls.node5
+
+    def test_get_path_to_target1(self) -> None:
+        path = get_path_to_target(root=self.root, target=self.root)
+        self.assertEqual(path, [1])
