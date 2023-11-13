19a955c6cc58f6dd1628fee84f203be5d8af8c69
Ryan Davis <rdavis@flexport.com>
2023-11-13 13:55:04 -0800

    k distance binary tree

---
+++ all_nodes_distance_k_binary_tree.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def get_distance_from_root(root: TreeNode, target: TreeNode)
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        pass
+
+
+class TestDistanceK(TestCase):
