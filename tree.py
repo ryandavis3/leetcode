5418da21a5221454578a9e5a100136efe16fd6fc
Ryan Davis <rdavis@flexport.com>
2023-11-13 14:37:56 -0800

    test

--- tree.py
+++ tree.py
@@ -15,15 +15,42 @@ class TreeNode:
 
 def get_tree_from_list(values: List[int]) -> TreeNode:
     root = TreeNode(x=values[0])
-    queue = Queue()
-    queue.put(root)
+    prev = Queue()
+    prev.put(root)
     level = 1
     start_index = int(math.pow(2, level-1))
     end_index = int(math.pow(2, level) + 1)
-    values_level = values[start_index:end_index]
+    x_level = values[start_index:end_index]
+    curr = Queue()
+    for x in x_level:
+        if x is None:
+            node = None
+        else:
+            node = TreeNode(x=x)
+        curr.put(node)
+    parent = prev.get()
+    while not curr.empty():
+        node = curr.get()
+        if parent.left is None:
+            parent.left = node
+        elif parent.right is None:
+            parent.right = node
+        else:
+            parent = prev.get()
+    return root
+
+
+
 
 
 class TestTree(TestCase):
     def test1(self) -> None:
         values = [3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]
         root = get_tree_from_list(values=values)
+
+    def test2(self) -> None:
+        values = [1, 2, 3]
+        root = get_tree_from_list(values=values)
+        self.assertEqual(root.val, 1)
+        self.assertEqual(root.left.val, 2)
+        self.assertEqual(root.right.val, 3)
