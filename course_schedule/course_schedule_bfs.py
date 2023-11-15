635fa38bf3653e779298876391843446a5552aa1
Ryan Davis <rdavis@flexport.com>
2023-11-15 11:50:48 -0800

    test bfs

--- course_schedule/course_schedule_bfs.py
+++ course_schedule/course_schedule_bfs.py
@@ -1,10 +1,11 @@
+from queue import Queue
 from unittest import TestCase
 from typing import Dict, List, Set
 
 
 class Courses:
     def __init__(self, num_courses: int, prerequisites: List[List[int]]):
-        self.visited = set()
+        self.num_courses = num_courses
         prerequisites_dict: Dict[int, Set] = {}
         for prerequisite in prerequisites:
             a, b = prerequisite
@@ -12,6 +13,28 @@ class Courses:
                 prerequisites_dict[a] = set()
             prerequisites_dict[a].add(b)
         self.prerequisites_dict = prerequisites_dict
+
+    def get_prerequisites(self, course: int) -> Set:
+        if course not in self.prerequisites_dict:
+            return set()
+        return self.prerequisites_dict[course]
+
+    def can_finish(self) -> bool:
+        visited = set()
+        queue = Queue()
+        root = self.num_courses - 1
+        queue.put(root)
+        while not queue.empty():
+            course = queue.get()
+            if course in visited:
+                return False
+            prereqs = self.get_prerequisites(course=course)
+            for prereq in prereqs:
+                if prereq in visited:
+                    continue
+                visited.add(prereq)
+                queue.put(prereq)
+        return True
 
 
 class Solution:
@@ -30,3 +53,5 @@ class TestCourses(TestCase):
         courses = Courses(num_courses=4, prerequisites=prerequisites)
         prerequisites_dict_expected = {1: {0}, 2: {0, 1}, 3: {2}}
         self.assertEqual(courses.prerequisites_dict, prerequisites_dict_expected)
+        can_finish = courses.can_finish()
+        self.assertTrue(can_finish)
