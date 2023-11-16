3cc079b9c4cf2769bbd8b9e31e71ab49f3bcb027
Ryan Davis <rdavis@flexport.com>
2023-11-16 12:08:51 -0800

    test

--- shortest_bridge.py
+++ shortest_bridge.py
@@ -1,4 +1,5 @@
-from typing import List
+from dataclasses import dataclass
+from typing import List, Set, Tuple
 from unittest import TestCase
 
 
@@ -26,19 +27,63 @@ def dfs(grid: List[List[int]], visited: List[List[int]], i: int, j: int) -> None
         dfs(grid=grid, visited=visited, i=1, j=j+1)
 
 
+@dataclass(frozen=True)
+class Islands:
+    island_1: Set[Tuple]
+    island_2: Set[Tuple]
+
+
+def get_coordinates_from_visited(visited: List[List[int]]) -> Set[Tuple]:
+    n = len(visited)
+    coordinates = set()
+    for i in range(n):
+        for j in range(n):
+            coordinates.add((i, j))
+    return coordinates
+
+
+def find_islands(grid: List[List[int]]) -> Islands:
+    n = len(grid)
+    islands_found = 0
+    visited_first = get_empty_matrix(n=n)
+    visited_second = get_empty_matrix(n=n)
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 1 and visited_first[i][j] == 0 and visited_second[i][j] == 0:
+                if islands_found == 0:
+                    dfs(grid=grid, visited=visited_first, i=i, j=j)
+                    island_1 = get_coordinates_from_visited(visited=visited_first)
+                    islands_found += 1
+                elif islands_found == 1:
+                    dfs(grid=grid, visited=visited_second, i=i, j=j)
+                    island_2 = get_coordinates_from_visited(visited=visited_second)
+                    islands_found += 1
+                else:
+                    raise ValueError('Already found two islands!')
+    islands = Islands(island_1=island_1, island_2=island_2)
+    return islands
+
+
+
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         pass
 
 
 class TestShortestBridge(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.grid = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+
     def test1(self) -> None:
-        grid = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
         visited = get_empty_matrix(n=3)
-        dfs(grid=grid, visited=visited, i=0, j=0)
+        dfs(grid=self.grid, visited=visited, i=0, j=0)
         visited_expected = [[1, 1, 0], [1, 1, 0], [0, 0, 0]]
         self.assertEqual(visited, visited_expected)
         visited = get_empty_matrix(n=3)
-        dfs(grid=grid, visited=visited, i=2, j=2)
+        dfs(grid=self.grid, visited=visited, i=2, j=2)
         visited_expected = [[0, 0, 0], [0, 0, 0], [0, 0, 1]]
         self.assertEqual(visited, visited_expected)
+
+    def test2(self) -> None:
+        islands = find_islands(grid=self.grid)
