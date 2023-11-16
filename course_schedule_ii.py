641fa811e5161e745ea4c89b4ef97e17212e8ef9
Ryan Davis <rdavis@flexport.com>
2023-11-15 18:31:38 -0800

    test

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -7,39 +7,54 @@ class Adjacency:
     def __init__(self, prerequisites: List[List[int]]):
         # Keys are prerequisites. Values are later courses
         # for which the key is a prerequisite.
-        adjacency_dict: Dict[int, Set] = {}
         prereq_dict: Dict[int, Set] = {}
+        follow_dict: Dict[int, Set] = {}
         for prerequisite in prerequisites:
             # b is the prerequisite for a
             a, b = prerequisite
-            if a not in adjacency_dict:
-                adjacency_dict[a] = set()
-            adjacency_dict[a].add(b)
-            if b not in prereq_dict:
-                prereq_dict[b] = set()
-            prereq_dict[b].add(a)
-        self.adjacency_dict = adjacency_dict
+            if a not in prereq_dict:
+                prereq_dict[a] = set()
+            prereq_dict[a].add(b)
+            if b not in follow_dict:
+                follow_dict[b] = set()
+            follow_dict[b].add(a)
         self.prereq_dict = prereq_dict
+        self.follow_dict = follow_dict
 
-    def apply_prereq(self, course: int, prereq: int) -> None:
-        if course not in self.adjacency_dict:
+    def apply_prereq(self, course: int, prereq: int) -> bool:
+        if course not in self.follow_dict:
             err_msg = f'Course {course} not in adjacency dictionary!'
             raise ValueError(err_msg)
-        if prereq not in self.adjacency_dict[course]:
+        if prereq not in self.follow_dict[course]:
             err_msg = f'Prereq {prereq} not listed for course {course}!'
             raise ValueError(err_msg)
-        self.adjacency_dict[course].remove(prereq)
+        self.follow_dict[course].remove(prereq)
+        if not self.follow_dict[course]:
+            del self.follow_dict[course]
+            return True
+        return False
 
+
+def find_order():
+    pass
 
 
 class TestAdjacency(TestCase):
     def test1(self) -> None:
-        courses = Adjacency(prerequisites=[[1, 0]])
-        prerequisites_dict_expected = {0: {1}}
-        self.assertEqual(courses.prereq_dict, prerequisites_dict_expected)
+        adjacency = Adjacency(prerequisites=[[1, 0]])
+        follow_dict_expected = {0: {1}}
+        self.assertEqual(adjacency.follow_dict, follow_dict_expected)
 
     def test2(self) -> None:
         prerequisites = [[1, 0], [2, 0], [2, 1], [3, 2]]
-        courses = Adjacency(prerequisites=prerequisites)
-        prerequisites_dict_expected = {0: {1, 2}, 1: {2}, 2: {3}}
-        self.assertEqual(courses.prereq_dict, prerequisites_dict_expected)
+        adjacency = Adjacency(prerequisites=prerequisites)
+        follow_dict_expected = {0: {1, 2}, 1: {2}, 2: {3}}
+        self.assertEqual(adjacency.follow_dict, follow_dict_expected)
+        prereq_dict_expected = {2: {0, 1}, 1: {0}, 3: {2}}
+        self.assertEqual(adjacency.prereq_dict, prereq_dict_expected)
+
+    def test3(self) -> None:
+        prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]]
+        adjacency = Adjacency(prerequisites=prerequisites)
+
+
