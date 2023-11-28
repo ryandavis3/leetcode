9fca66036b9a3d97b12296bbf18215a17812d5b8
Ryan Davis <rdavis@flexport.com>
2023-11-27 16:18:58 -0800

    remove boxes

--- remove_boxes.py
+++ remove_boxes.py
@@ -1,6 +1,11 @@
 from typing import List
 
 
+class Boxes:
+    def __init__(self, boxes: List[int]):
+        self.boxes = boxes
+
+
 class Solution:
     def removeBoxes(self, boxes: List[int]) -> int:
         pass
