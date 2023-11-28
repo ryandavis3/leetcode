18a2f7f116532120ec90c7095347d2efcfdcd327
Ryan Davis <rdavis@flexport.com>
2023-11-27 16:24:17 -0800

    test

--- remove_boxes.py
+++ remove_boxes.py
@@ -1,11 +1,30 @@
+from unittest import TestCase
 from typing import List
 
 
 class Boxes:
     def __init__(self, boxes: List[int]):
-        self.boxes = boxes
+        self._boxes = boxes
+        self._box_locations: Dict[int, Set]
+
+
+
+def remove_boxes(boxes: List[int]) -> int:
+    pass
 
 
 class Solution:
     def removeBoxes(self, boxes: List[int]) -> int:
         pass
+
+
+class TestRemoveBoxes(TestCase):
+    def test1(self) -> None:
+        boxes = [1, 1, 1]
+        output = remove_boxes(boxes=boxes)
+        self.assertEqual(output, 9)
+
+    def test2(self) -> None:
+        boxes = [1]
+        output = remove_boxes(boxes=boxes)
+        self.assertEqual(output, 1)
