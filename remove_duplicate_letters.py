8679aefecf0b57e8a1ff12c0f5423001b6b6d949
Ryan Davis <rdavis@flexport.com>
2023-12-26 10:57:49 -0500

    remove dups

--- remove_duplicate_letters.py
+++ remove_duplicate_letters.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from unittest import TestCase
 
 
@@ -7,12 +8,23 @@ class Solution:
 
 
 def remove_duplicate_letters(s: str) -> str:
-    pass
+    if s == '':
+        return ''
+    counter = Counter(s)
+    pos = 0
+    for i, char in enumerate(s):
+        if char < s[pos]:
+            pos = i
+        counter[char] -= 1
+        if counter[char] == 0:
+            break
+    letter = s[pos]
+    s_remain = s[pos+1:].replace(letter, '')
+    return letter + remove_duplicate_letters(s=s_remain)
 
 
 class TestDuplicateLetters(TestCase):
     def test1(self) -> None:
-        return None
         test_cases = {
             'bcabc': 'abc',
             'cbacdcbc': 'acdb',
