06d945025759387687e7dec6f8e63a8481208c97
Ryan Davis <rdavis@flexport.com>
2023-12-27 14:53:10 -0500

    fmf

---
+++ frequency_most_frequent_element2.py
@@ -0,0 +1,12 @@
+from typing import List
+from unittest import TestCase
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        pass
+
+
+class TestMaxFreq(TestCase):
+    def test1(self) -> None:
+        pass
