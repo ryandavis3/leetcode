92ff815e5250ae03b7a7de740fa6326dccc9247d
Ryan Davis <rdavis@flexport.com>
2023-12-27 18:24:27 -0500

    hc

--- hit_counter.py
+++ hit_counter.py
@@ -22,7 +22,10 @@ class HitCounter:
 
     def apply_timestamp(self, timestamp: int) -> None:
         cutoff_timestamp = timestamp - self.lookback_seconds
+        if self.tail is not None:
+            print(self.tail.val)
         while self.tail is not None and self.tail.val <= cutoff_timestamp:
+            print(self.tail.val)
             self.tail = self.tail.next
             self.n_hits -= 1
 
@@ -34,6 +37,8 @@ class HitCounter:
         self.apply_timestamp(timestamp=timestamp)
         self.head.next = node
         self.head = node
+        if self.tail is None:
+            self.tail = node
         self.n_hits += 1
 
     def getHits(self, timestamp: int) -> int:
@@ -54,3 +59,37 @@ class TestHitCounter(TestCase):
         self.assertEqual(hits, 4)
         hits = counter.getHits(timestamp=301)
         self.assertEqual(hits, 3)
+
+    def test2(self) -> None:
+        counter = HitCounter()
+        counter.hit(timestamp=100)
+        counter.hit(timestamp=151)
+        hits = counter.getHits(timestamp=173)
+        self.assertEqual(hits, 2)
+        hits = counter.getHits(timestamp=179)
+        self.assertEqual(hits, 2)
+        counter.hit(timestamp=188)
+        hits = counter.getHits(timestamp=250)
+        self.assertEqual(hits, 3)
+        hits = counter.getHits(timestamp=267)
+        self.assertEqual(hits, 3)
+        hits = counter.getHits(timestamp=396)
+        self.assertEqual(hits, 3)
+        hits = counter.getHits(timestamp=410)
+        self.assertEqual(hits, 2)
+        counter.hit(timestamp=547)
+        counter.hit(timestamp=552)
+        counter.hit(timestamp=605)
+        counter.hit(timestamp=690)
+        counter.hit(timestamp=707)
+        hits = counter.getHits(timestamp=713)
+        self.assertEqual(hits, 5)
+        hits = counter.getHits(timestamp=797)
+        self.assertEqual(hits, 5)
+        hits = counter.getHits(timestamp=808)
+        self.assertEqual(hits, 5)
+        counter.hit(timestamp=859)
+        hits = counter.getHits(timestamp=943)
+        self.assertEqual(hits, 3)
+        counter.hit(timestamp=956)
+        counter.hit(timestamp=957)
