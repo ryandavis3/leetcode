e1fc95d52febdda9e62e559465240d7e012c4b71
Ryan Davis <rdavis@flexport.com>
2023-12-29 16:20:01 -0500

    min deletions

---
+++ min_deletions_chars_unique.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from heapq import heapify, heappop, heappush
+
+
+def get_min_deletions(s: str) -> int:
+    counter = Counter(s)
+    pq = [(-count, char) for char, count in counter.items()]
+    heapify(pq)
+    n_deletions = 0
+    while pq:
+        count_first, char_first = heappop(pq)
+        if not pq:
+            break
+        if pq[0] == count_first:
+            i = 0
+            while pq[i][0] == count_first:
+                i += 1
+            for _ in range(i):
+                count, char = heappop(pq)
+                n_deletions += 1
+                if -count + 1 != 0:
+                    heappush(pq, (-count + 1))
+    return n_deletions
+
+
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        pass
