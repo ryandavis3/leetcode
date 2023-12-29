9e9adc04ab6cb10abbcb0fd967a545e8bcf2f97a
Ryan Davis <rdavis@flexport.com>
2023-12-29 16:23:09 -0500

    test

--- min_deletions_chars_unique.py
+++ min_deletions_chars_unique.py
@@ -1,3 +1,4 @@
+from unittest import TestCase
 from collections import Counter
 from heapq import heapify, heappop, heappush
 
@@ -8,22 +9,29 @@ def get_min_deletions(s: str) -> int:
     heapify(pq)
     n_deletions = 0
     while pq:
+        print(pq)
         count_first, char_first = heappop(pq)
         if not pq:
             break
-        if pq[0] == count_first:
+        if pq[0][0] == count_first:
             i = 0
-            while pq[i][0] == count_first:
+            while i < len(pq) and pq[i][0] == count_first:
                 i += 1
             for _ in range(i):
                 count, char = heappop(pq)
                 n_deletions += 1
-                if -count + 1 != 0:
-                    heappush(pq, (-count + 1))
+                if count + 1 != 0:
+                    heappush(pq, (count + 1, char))
     return n_deletions
-
 
 
 class Solution:
     def minDeletions(self, s: str) -> int:
         pass
+
+
+class TestMinDeletions(TestCase):
+    def test1(self) -> None:
+        s = 'aaabbbcc'
+        min_deletions = get_min_deletions(s=s)
+        self.assertEqual(min_deletions, 2)
