fab56826e757c4700061f119d82b0dfa0dcbf731
Ryan Davis <rdavis@flexport.com>
2023-12-28 20:19:42 -0500

    test

--- is_robot_bounded.py
+++ is_robot_bounded.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 
+G = 'G'
 L = 'L'
 R = 'R'
 
@@ -12,12 +13,26 @@ class Solution:
 
 def is_robot_bounded(instructions: str) -> bool:
     degree = 0
+    x = 0
+    y = 0
     for instruction in instructions:
         if instruction == L:
-            degree += 1
+            degree = (degree + 1) / 4
         elif instruction == R:
-            degree -= 1
-    return (degree % 4) != 0
+            degree = (degree - 1) / 4
+        if instruction == G:
+            # north
+            if degree == 0:
+                y += 1
+            # west
+            elif degree == 1:
+                x -= 1
+            # south
+            elif degree == 2:
+                y -= 1
+            else:
+                x += 1
+    return (degree % 4) != 0 or (x==0 and y==0)
 
 
 class TestRobotBounded(TestCase):
@@ -25,7 +40,8 @@ class TestRobotBounded(TestCase):
         test_cases = {
             'GGLLGG': True,
             'GG': False,
-            'GL': True
+            'GL': True,
+            'GLRLLGLL': True
         }
         for instructions, expected in test_cases.items():
             result = is_robot_bounded(instructions=instructions)
