868f2930b50f3ce590be1cfdb690c63f61d70a8d
Ryan Davis <rdavis@flexport.com>
2023-12-30 11:06:23 -0500

    music playlists

--- number_music_playlists.py
+++ number_music_playlists.py
@@ -1,3 +1,20 @@
+from typing import List
+from unittest import TestCase
+
+
+MOD = 10**9 + 7
+
+
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        pass
+
+
+def get_num_music_playlists(n: int, goal: int, k: int) -> int:
+    # Initialize a goal x n DP table.
+    dp: List[List[int]] = []
+
+
+class TestMusicPlaylists(TestCase):
+    def test1(self) -> None:
         pass
