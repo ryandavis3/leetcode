29d6ed60031780c89cbce73cb3891a75206959f5
Ryan Davis <rdavis@flexport.com>
2023-12-29 20:45:26 -0500

    dq

--- max_sliding_window.py
+++ max_sliding_window.py
@@ -9,7 +9,25 @@ class Solution:
 
 
 def get_max_sliding_window(nums: List[int], k: int) -> List[int]:
-    pass
+    # First window
+    dq = deque()
+    n = len(nums)
+    res = [0] * (n - k + 1)
+    for i in range(k):
+        while dq and nums[i] >= nums[dq[-1]]:
+            dq.pop()
+        dq.append(i)
+    res[0] = nums[dq[0]]
+    # Sliding window
+    for i in range(k, n):
+        print(dq)
+        if dq[0] == i - k:
+            dq.popleft()
+        while dq and nums[i] >= nums[dq[-1]]:
+            dq.pop()
+        dq.append(i)
+        res[i-k+1] = nums[dq[0]]
+    return res
 
 
 class TestMaxSlidingWindow(TestCase):
