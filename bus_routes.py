14b5471561acb56f1286c9e552f463c8f37ae9d2
Ryan Davis <rdavis@flexport.com>
2023-12-30 15:10:57 -0500

    test

--- bus_routes.py
+++ bus_routes.py
@@ -2,6 +2,9 @@ from unittest import TestCase
 from typing import Dict, List, Set
 
 
+LARGE = 10 ** 10
+
+
 class Solution:
 
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
@@ -51,6 +54,27 @@ def get_buses_to_target(source_route: int, target_route: int, route_to_routes_di
     return 1 + min(reachable_paths)
 
 
+def get_num_buses_to_destination(routes: List[List[int]], source: int, target: int) -> int:
+    stop_to_routes = get_stop_to_routes_dict(routes=routes)
+    route_to_routes_dict = get_route_to_routes_dict(routes=routes)
+    source_routes = stop_to_routes[source]
+    target_routes = stop_to_routes[target]
+    if source_routes & target_routes:
+        return 0
+    min_buses = LARGE
+    for source_route in source_routes:
+        for target_route in target_routes:
+            buses = get_buses_to_target(source_route=source_route,
+                                        target_route=target_route,
+                                        route_to_routes_dict=route_to_routes_dict,
+                                        prev_routes=set())
+            if buses == -1:
+                continue
+            elif buses < min_buses:
+                min_buses = buses
+    return min_buses
+
+
 class TestBuses(TestCase):
 
     @classmethod
@@ -95,3 +119,7 @@ class TestBuses(TestCase):
                                     route_to_routes_dict=route_to_routes,
                                     prev_routes=set())
         self.assertEqual(buses, 3)
+
+    def test5(self) -> None:
+        buses = get_num_buses_to_destination(source=1, target=3, routes=self.routes)
+        self.assertEqual(buses, 1)
