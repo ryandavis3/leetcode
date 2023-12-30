42125343b508ab53d3e0333fef8bfb3cc2a75f48
Ryan Davis <rdavis@flexport.com>
2023-12-30 14:33:16 -0500

    bus routes test

--- bus_routes.py
+++ bus_routes.py
@@ -1,6 +1,37 @@
-from typing import List
+from unittest import TestCase
+from typing import Dict, List
 
 
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         pass
+
+
+def get_stop_to_routes_dict(routes: List[List[int]]) -> Dict[int, List[int]]:
+    stop_to_routes: Dict[int, List[int]] = {}
+    for route_index, route_stops in enumerate(routes):
+        for stop in route_stops:
+            if stop not in stop_to_routes:
+                stop_to_routes[stop] = list()
+            stop_to_routes[stop].append(route_index)
+    return stop_to_routes
+
+
+def get_num_buses_to_destination(routes: List[List[int]], source: int, target: int) -> int:
+    pass
+
+
+class TestBuses(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.routes = [[1, 2, 7], [3, 6, 7]]
+
+    def test1(self) -> None:
+        stop_to_routes = get_stop_to_routes_dict(routes=self.routes)
+        stop_to_routes_expected = {
+            1: [0],
+            2: [0],
+            7: [0, 1],
+            3: [1],
+            6: [1],
+        }
